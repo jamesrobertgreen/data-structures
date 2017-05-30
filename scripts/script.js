7586bda63dc6d57a9b7ad83704ffa8d755918ff4
James Green <jamesg@ensemble.com>
2017-05-30 13:47:22 +0100

    Add search entity details

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,38 @@
 	var routeApp = angular.module('dataStructureApp', []);
 	routeApp.controller('mainController', function ($scope) {
-        $scope.displayJSON = function(obj){
-            return JSON.stringify(obj, null, 4);
-        };
+
+
+	    $scope.displayJSON = function (obj) {
+	        return JSON.stringify(obj, null, 4);
+	    };
 	    $scope.listCases = {
 	        "cases": [{
-	            "caseId": "001"
-	            , "caseStatus": "Initial Request Incomplete"
-	            , "caseOwner": "Nawaf Hasan"
-	            , "caseCreationDate": "20/05/17"
-	            , "action": "Mark as Signed"
-	            , "entities": [{
-	                "entityName": "Reebok UK"
-	                , "entityId": "890123"
-                }]
-	            , "products": [{
-	                "productId": "987"
-	                , "productDescription": "Account (UK)"
-	                , "owningEntityId": "890123"
+	            "caseId": "001",
+	            "caseStatus": "Initial Request Incomplete",
+	            "caseOwner": "Nawaf Hasan",
+	            "caseCreationDate": "20/05/17",
+	            "action": "Mark as Signed",
+	            "entities": [{
+	                "entityName": "Reebok UK",
+	                "entityId": "890123"
+                }],
+	            "products": [{
+	                "productId": "987",
+	                "productDescription": "Account (UK)",
+	                "owningEntityId": "890123"
                             }, {
-	                "productId": "655"
-	                , "productDescription": "Echannel (UK)"
-	                , "owningEntityId": "890123"
-                }]
-	            , "associatedCases": [{
-	                "associationType": "child"
-	                , "caseId": "002"
+	                "productId": "655",
+	                "productDescription": "Echannel (UK)",
+	                "owningEntityId": "890123"
+                }],
+	            "associatedCases": [{
+	                "associationType": "child",
+	                "caseId": "002"
 	           }]
 	       }]
 	    };
-        $scope.listCasesStr = JSON.stringify($scope.listCases, null, 4);
-        
+
+
 	    // if we have the above, then it will be fine *as long as we  only receive one entity per case*....as otherwise I will have to go through and check who is the
 	    // owning entity in the list of products to match them up on the UI.
 	    // some other considerations if we go down this route:
@@ -42,30 +44,30 @@
 	    // do we remove the accounts/e-channels that are associated with that entity?
 	    $scope.listCases2 = {
 	        "cases": [{
-	            "caseId": "001"
-	            , "caseStatus": "Initial Request Incomplete"
-	            , "caseOwner": "Nawaf Hasan"
-	            , "caseCreationDate": "20/05/17"
-	            , "action": "Mark as Signed"
-	            , "entities": [{
-	                "entityName": "Reebok UK"
-	                , "entityId": "890123"
-	                , "products": [{
-	                    "productId": "987"
-	                    , "productDescription": "Account (UK)"
+	            "caseId": "001",
+	            "caseStatus": "Initial Request Incomplete",
+	            "caseOwner": "Nawaf Hasan",
+	            "caseCreationDate": "20/05/17",
+	            "action": "Mark as Signed",
+	            "entities": [{
+	                "entityName": "Reebok UK",
+	                "entityId": "890123",
+	                "products": [{
+	                    "productId": "987",
+	                    "productDescription": "Account (UK)"
                             }, {
-	                    "productId": "655"
-	                    , "productDescription": "Echannel (UK)"
+	                    "productId": "655",
+	                    "productDescription": "Echannel (UK)"
                     }]
                     }, {
-	                "entityName": "Reebok FR"
-	                , "entityId": "890168"
-	                , "products": [{
-	                    "productId": "567"
-	                    , "productDescription": "Account (UK)"
+	                "entityName": "Reebok FR",
+	                "entityId": "890168",
+	                "products": [{
+	                    "productId": "567",
+	                    "productDescription": "Account (UK)"
                             }, {
-	                    "productId": "435"
-	                    , "productDescription": "Echannel (UK)"
+	                    "productId": "435",
+	                    "productDescription": "Echannel (UK)"
                     }]
                 }]
 	       }]
@@ -73,46 +75,55 @@
 	    $scope.createCase = {
 	        "caseId": "005"
 	    };
-        
+
 	    $scope.searchEntities = {
 	        "entities": [{
-	                "entityId": "123456"
-	                , "entityName": "Crocs Footwear UK LTD"
+	                "entityId": "123456",
+	                "entityName": "Crocs Footwear UK LTD"
 	            }
 	            , {
-	                "entityId": "234567"
-	                , "entityName": "Crocs Footwear France S.A"
+	                "entityId": "234567",
+	                "entityName": "Crocs Footwear France S.A"
 	            }
 	            , {
-	                "entityId": "456789"
-	                , "entityName": "Crocs Fashion Spain S.A"
+	                "entityId": "456789",
+	                "entityName": "Crocs Fashion Spain S.A"
 	            }]
 	    };
-        $scope.searchEntitiesStr = JSON.stringify($scope.createCase, null, 4);
-        
+
+	    $scope.searchEntities_addSelected = {
+	        "entities": [{
+	            "entityId": "123456"
+	        }, {
+	            "entityId": "234567"
+	        }]
+	    };
+
+
+
 	    $scope.entityList = {
 	        "entities": [{
-	                "entityId": "123456"
-	                , "entityName": "Crocs Footwear UK LTD"
-	                , "conflicts": [{
+	                "entityId": "123456",
+	                "entityName": "Crocs Footwear UK LTD",
+	                "conflicts": [{
 	                    "caseId": "005"
 	                        }, {
 	                    "caseId": "006"
 	                        }]
 	            }
 	            , {
-	                "entityId": "234567"
-	                , "entityName": "Crocs Footwear France S.A"
-	                , "conflicts": [{
+	                "entityId": "234567",
+	                "entityName": "Crocs Footwear France S.A",
+	                "conflicts": [{
 	                    "caseId": "007"
 	                        }, {
 	                    "caseId": "008"
 	                        }]
 	            }
 	            , {
-	                "entityId": "456789"
-	                , "entityName": "Crocs Fashion Spain S.A"
-	                , "conflicts": [{
+	                "entityId": "456789",
+	                "entityName": "Crocs Fashion Spain S.A",
+	                "conflicts": [{
 	                    "caseId": "009"
 	                        }, {
 	                    "caseId": "010"
@@ -122,30 +133,30 @@
 	            }]
 	    };
 	    $scope.entityDetails = {
-	        "entityId": "001"
-	        , "entityName": "Crocs footwear UK LTD"
-	        , "entityAddress": "10 King Street"
+	        "entityId": "001",
+	        "entityName": "Crocs footwear UK LTD",
+	        "entityAddress": "10 King Street"
 	    };
 	    $scope.gdocsHistory = {
 	        "cases": [{
-	                "caseId": "001"
-	                , "service": "Account opening"
-	                , "country": "Argentina"
-	                , "date": "13 Jun '16"
-	                , "outputPackRef": "1234567"
-	                , "documents": [{
+	                "caseId": "001",
+	                "service": "Account opening",
+	                "country": "Argentina",
+	                "date": "13 Jun '16",
+	                "outputPackRef": "1234567",
+	                "documents": [{
 	                    "documentName": "MSA"
 	                        }, {
 	                    "documentName": "RAF"
 	                        }]
 	                    }
 	                    , {
-	                "caseId": "002"
-	                , "service": " E-channel"
-	                , "country": "Argentina"
-	                , "date": "13 Jun '16"
-	                , "outputPackRef": "1234567"
-	                , "documents": [{
+	                "caseId": "002",
+	                "service": " E-channel",
+	                "country": "Argentina",
+	                "date": "13 Jun '16",
+	                "outputPackRef": "1234567",
+	                "documents": [{
 	                    "documentName": "doc1"
 	                        }, {
 	                    "documentName": "doc2"
